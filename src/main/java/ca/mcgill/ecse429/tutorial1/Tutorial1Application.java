37dc5a24b92902b430022d91baa18fbaafb0b597
Julian Armour <julian.armour@videotron.ca>
2019-09-20 09:52:31 -0400

    created spring project

---
+++ src/main/java/ca/mcgill/ecse429/tutorial1/Tutorial1Application.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse429.tutorial1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial1Application.class, args);
+	}
+
+}
